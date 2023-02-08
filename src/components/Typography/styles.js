76f4fad4faa6602d8eb65e69bd99e20855a2a7f2
Renan Oliveira <oliveira.renan.rg@gmail.com>
2023-02-07 21:22:12 -0300

    feat: Implementa Cabeçalho

---
+++ src/components/Typography/styles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+const Typography = styled('span').attrs(({ tag }) => ({
+  as: tag,
+}))`
+  color: ${({ theme, color }) => {
+    if (color) return theme.colors[color];
+    return 'inherit';
+  }
+};
+  font-size: ${({ theme, type }) => theme.typography.types[type].size};
+  font-weight: ${({ theme, weight }) => {
+    if (weight) return theme.weight[weight];
+    return '400';
+  }
+};
+  line-height: ${({ theme, type }) => theme.typography.types[type].lineHeight};
+  opacity: ${({ disabled }) => {
+    if (disabled) return '0.4';
+    return '1';
+  }};
+  transition: all .4s ease;
+`;
+
+export default Typography;
---
+++ src/styles/theme.js
@@ -0,0 +1,61 @@
+const theme = {
+  colors: {
+    primary: '#FFFFFF',
+    secondary: '#1A1A1A',
+    tertiary: '#CD0028',
+  },
+  weight: {
+    regular: '400',
+    medium: '500',
+    bold: '700',
+  },
+  fonts: {
+    sans: '"DM Sans", sans-serif;',
+  },
+  typography: {
+    types: {
+      'display-01': {
+        size: '4.0rem',
+        lineHeight: '4.8rem',
+      },
+      'display-02': {
+        size: '3.2rem',
+        lineHeight: '4.0rem',
+      },
+      'heading-01': {
+        size: '2.8rem',
+        lineHeight: '3.6rem',
+      },
+      'heading-02': {
+        size: '2.4rem',
+        lineHeight: '3.2rem',
+      },
+      'heading-03': {
+        size: '2rem',
+        lineHeight: '2.8rem',
+      },
+      'heading-04': {
+        size: '1.8rem',
+        lineHeight: '2.6rem',
+      },
+      'body-01': {
+        size: '1.4rem',
+        lineHeight: '2.4rem',
+      },
+      'body-02': {
+        size: '16',
+        lineHeight: '16px',
+      },
+      'caption-01': {
+        size: '0.8rem',
+        lineHeight: '1.6rem',
+      },
+      'caption-02': {
+        size: '10px',
+        lineHeight: '16px',
+      },
+    },
+  },
+};
+
+export default theme;
